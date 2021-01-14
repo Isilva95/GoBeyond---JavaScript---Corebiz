9873dcd55e18ca0ff692e0169a8049942484104d
Isac Santos <isilva_santos@hotmail.com>
2021-01-13 22:58:54 -0300

    JavaScript Puro

---
+++ Test/app.js
@@ -0,0 +1,26 @@
+var botao = document.getElementById("botao");
+
+function teste() {
+  // Variaveis
+
+  var nome = "Gabriel";
+  var idade = 20;
+  var estudante = true;
+
+  // Operadores
+
+  var num1 = 5;
+  var num2 = 3;
+
+  var soma = num1 + num2;
+
+  // Condicionais
+
+  if(idade === 20 || nome === "Jorge") {
+    console.log(" é igual")
+  }else{
+    console.log("não é igual")
+  }
+}
+
+teste();
